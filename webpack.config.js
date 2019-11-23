39a3ffb6aae1f55f0dc9b0a843b036a8b8567474
Belaid Alahian <alahian.belaid@gmail.com>
2019-11-23 03:48:44 +0100

    use optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
 
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
         publicPath:'/dist'
-    } 
+    } ,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimize:true,
+        minimizer: [new TerserPlugin()]
+      }
 };
